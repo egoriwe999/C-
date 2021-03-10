2dbf614bfe7128f389da7f119507981f4cd4c813
egoriwe999 <emernolds@gmail.com>
2021-03-10 19:25:10 +0200

    Initial commit

---
+++ Фирмы.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    int mxcnt = -1; 
+    int id = -1; 
+    for (int i = 0; i < t; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (a * b > mxcnt)
+        {
+            mxcnt = a * b;
+            id = i + 1;
+        }
+    }
+    cout << id;
+}
+// (c) egoriwe999 
+    // my telegram: @egoriwe999
+
